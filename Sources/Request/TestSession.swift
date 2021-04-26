cf2c368fcc6b1c4c36a08e666f6f211c2f2e2cd2
Steve Madsen <steve@lightyearsoftware.com>
2021-04-26 11:42:43 -0400

    Move into main module
    Users of the module can then use TestSession in their own tests.

--- Sources/Request/TestSession.swift
+++ Sources/Request/TestSession.swift
@@ -6,9 +6,8 @@
 //
 
 import Foundation
-@testable import Request
 
-class TestSession: RequestSession {
+open class TestSession: RequestSession {
     struct Request: Hashable {
         let method: HTTPMethod
         let url: String
@@ -29,23 +28,26 @@ class TestSession: RequestSession {
 
     private var stubRequests = [Request: Response]()
 
-    func reset() {
+    public init() {
+    }
+    
+    public func reset() {
         stubRequests = [:]
     }
 
-    func allow(_ method: HTTPMethod, _ url: String, return status: Int, headers: [String: String] = ["Content-Type": "application/json"], body: String = "") {
+    public func allow(_ method: HTTPMethod, _ url: String, return status: Int, headers: [String: String] = ["Content-Type": "application/json"], body: String = "") {
         stubRequests[Request(method: method, url: url)] = Response(status: status, headers: headers, body: body)
     }
 
-    func allow(_ method: HTTPMethod, _ url: String, return error: Error) {
+    public func allow(_ method: HTTPMethod, _ url: String, return error: Error) {
         stubRequests[Request(method: method, url: url)] = .networkError(error)
     }
 
-    func dataTask(with request: URLRequest) -> URLSessionDataTask {
+    public func dataTask(with request: URLRequest) -> URLSessionDataTask {
         fatalError("not implemented")
     }
 
-    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+    public func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
         let req = Request(method: HTTPMethod(rawValue: request.httpMethod!)!, url: request.url!.absoluteString)
         if case .http(let status, let headers, let body) = stubRequests[req] {
             let httpResponse = HTTPURLResponse(url: request.url!, statusCode: status, httpVersion: "1.1", headerFields: headers.merging(["Content-Length": "\(body.count)"]) { _, other in other })
